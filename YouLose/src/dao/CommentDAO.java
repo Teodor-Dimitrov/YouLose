a365650ea7e6f7e0f94bcb96bda7e33a3a56e212
Natalie <natalie_hristova@abv.bg>
2017-04-18 11:04:30 +0300

    deleting try/catch in dao ,putting throws

--- YouLose/src/dao/CommentDAO.java
+++ YouLose/src/dao/CommentDAO.java
@@ -1,18 +1,67 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import model.Comment;
+import model.User;
+import model.Video;
 
 public class CommentDAO {
 	private static CommentDAO instance;
-	private static final HashMap<String, Comment> allVideos = new HashMap<>();//
-
+	private static final HashSet< Comment> allComments = new HashSet<>();//
 
 	public synchronized CommentDAO getInstance() {
 		if (instance == null) {
 			instance = new CommentDAO();
 		}
 		return instance;
+	}
+
+	public HashSet< Comment> getAllComments() throws SQLException {
+		if (allComments.isEmpty()) {
+			String sql = "SELECT users_user_id,video_id,content,postedDate FROM comments;";
+				PreparedStatement st;
+				
+					st = DBManager.getInstance().getConnection().prepareStatement(sql);
+					ResultSet res = st.executeQuery();
+					while (res.next()) {
+						Comment comment = new Comment();
+						comment.setContent(res.getString("content"));
+						comment.setUserID(res.getInt("users_user_id"));
+						comment.setVideoID(res.getInt("video_id"));
+						comment.setPostedDate(res.getTimestamp("postedDate").toLocalDateTime());
+						allComments.add(comment);
+					}
+				
+
+			}
+			return allComments;
+			// TODO
+		}
+	
+
+	public void commentVideo(Comment comment, Video video, User user) throws SQLException {
+		// TODO DA OPRAVIM V DB nqkoi danni za comment
+		Connection con = DBManager.getInstance().getConnection();
+		PreparedStatement st;
+		
+			st = con.prepareStatement(
+					"INSERT INTO comments (users_user_id,video_id,content,postedDate) VALUES (?,?,?,?);");
+			st.setInt(1, user.getUserID());
+			st.setInt(2, video.getId());
+			st.setString(3, comment.getContent());
+			// TODO DATE THINGS
+			// st.setTimestamp(4,comment.getPostedDate());
+			st.executeUpdate();
+		
+	}
+	
+	public void deleteComment(Comment comment){
+		
 	}
 }
--- YouLose/src/dao/VideoDAO.java
+++ YouLose/src/dao/VideoDAO.java
@@ -24,11 +24,11 @@ public class VideoDAO {
 		return instance;
 	}
 
-	public HashMap<String, Video> getAllVideos() {
+	public HashMap<String, Video> getAllVideos() throws SQLException {
 		String sql = "SELECT videos_id, name, path, views,date,description FROM videos;";
 		if (allVideos.isEmpty()) {
 			PreparedStatement st;
-			try {
+		
 				st = DBManager.getInstance().getConnection().prepareStatement(sql);
 				ResultSet res = st.executeQuery();
 				while (res.next()) {
@@ -41,21 +41,17 @@ public class VideoDAO {
 					video.setDescription(res.getString("description"));
 					allVideos.put(video.getName(), video);
 				}
-			} catch (SQLException e) {
-				System.out.println("problem in getting a video ");
-				e.printStackTrace();
-			}
-
+		
 		}
 		return allVideos;
 	}
 
-	public HashMap<String, Video> searchAllByString(String partOfName) {
+	public HashMap<String, Video> searchAllByString(String partOfName) throws SQLException {
 		String sql = "SELECT videos_id, name, path, views,date,description FROM videos WHERE name " + "like '"
 				+ partOfName + "';";
 		if (allVideos.isEmpty()) {
 			PreparedStatement st;
-			try {
+		
 				st = DBManager.getInstance().getConnection().prepareStatement(sql);
 				ResultSet res = st.executeQuery();
 				while (res.next()) {
@@ -68,20 +64,16 @@ public class VideoDAO {
 					video.setDescription(res.getString("description"));
 					allVideos.put(video.getName(), video);
 				}
-			} catch (SQLException e) {
-				System.out.println("problem in getting a video by this search");
-				e.printStackTrace();
-			}
 
 		}
 		return allVideos;
 	}
 
-	public void addVideo(Video video, User user) {// TODO ADD PUBLISHER TO DB
+	public void addVideo(Video video, User user) throws SQLException {// TODO ADD PUBLISHER TO DB
 													// ???
 		Connection con = DBManager.getInstance().getConnection();
 		PreparedStatement st;
-		try {
+		
 			st = con.prepareStatement("INSERT INTO videos (name, path, views,date,description) VALUES (?,?,?,?,?);");
 			st.setString(1, video.getName());
 			st.setString(2, video.getName());
@@ -90,17 +82,14 @@ public class VideoDAO {
 			// st.setPublisher(.....,video.getPublisher());
 			st.setString(5, video.getDescription());
 			st.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("not added to db ");
-			e.printStackTrace();
-		}
+		
 
 	}
 
-	public void likeVideo(Video video, User user) {
+	public void likeVideo(Video video, User user) throws SQLException {
 		Connection con = DBManager.getInstance().getConnection();
 		PreparedStatement st;
-		try {
+	
 			st = con.prepareStatement(
 					"INSERT INTO video_liked_or_disliked (users_user_id, videos_videos_id,like_or_dislike) VALUES (?,?,?);");
 			st.setInt(1, video.getId());
@@ -108,17 +97,13 @@ public class VideoDAO {
 			// TODO LIKES OR DISLIKES ?? LIKES -1 DISLIKES -2
 			st.setInt(3, 1);
 			st.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println(" NOT liked");
-			e.printStackTrace();
-		}
-
+		
 	}
 
-	public void dislikeVideo(Video video, User user) {
+	public void dislikeVideo(Video video, User user) throws SQLException {
 		Connection con = DBManager.getInstance().getConnection();
 		PreparedStatement st;
-		try {
+	
 			st = con.prepareStatement(
 					"INSERT INTO video_liked_or_disliked (users_user_id, videos_videos_id,like_or_dislike) VALUES (?,?,?);");
 			st.setInt(1, video.getId());
@@ -126,24 +111,18 @@ public class VideoDAO {
 			// TODO LIKES OR DISLIKES ?? LIKES -1 DISLIKES -2
 			st.setInt(3, 2);
 			st.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println(" NOT disliked");
-			e.printStackTrace();
-		}
+		
 
 	}
 
-	public void removeLikeFromVideo(Video video, User user) {
+	public void removeLikeFromVideo(Video video, User user) throws SQLException {
 		Connection con = DBManager.getInstance().getConnection();
 		PreparedStatement st;
-		try {
+		
 			st = con.prepareStatement("DELETE FROM video_liked_or_disliked WHERE users_user_id=" + user.getUserID()
 					+ " AND videos_videos_id=" + video.getId() + " AND like_or_dislike=1 ;");
 			st.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println(" NOT deleted like");
-			e.printStackTrace();
-		}
+		
 	}
 
 	public void removeDislikeFromVideo(Video video, User user) {
@@ -159,95 +138,56 @@ public class VideoDAO {
 		}
 	}
 
-	public void deleteVideo(Video video, User user) {
+	public void deleteVideo(Video video, User user) throws SQLException {
 		if (user.getUserID() == video.getPublisher()) {
 			Connection con = DBManager.getInstance().getConnection();
 			PreparedStatement st;
-			try {
 				st = con.prepareStatement("DELETE FROM videos WHERE videos_id=" + video.getId() + ";");
 				st.executeUpdate();
-			} catch (SQLException e) {
-				System.out.println(" NOT deleted video");
-				e.printStackTrace();
-			}
 		}
 	}
 
-	public void addVideoToPlayList(Video video, Playlist playlist) {
+	public void addVideoToPlayList(Video video, Playlist playlist) throws SQLException {
 		Connection con = DBManager.getInstance().getConnection();
 		PreparedStatement st;
-		try {
+		
 			st = con.prepareStatement(
 					"INSERT INTO playlist_videos (videos_videos_id,playlists_playlist_id) VALUES (?,?);");
 			st.setInt(1, video.getId());
 			st.setInt(2, playlist.getPlaylistID());
 			st.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println(" NOT video added to playlist");
-			e.printStackTrace();
-		}
+	
 	}
 
-	public void removeVideoFromPlaylist(Video video, Playlist playlist) {
+	public void removeVideoFromPlaylist(Video video, Playlist playlist) throws SQLException {
 		Connection con = DBManager.getInstance().getConnection();
 		PreparedStatement st;
-		try {
+		
 			st = con.prepareStatement("DELETE FROM playlist_videos WHERE videos_videos_id= " + video.getId()
 					+ " playlists_playlist_id=" + playlist.getPlaylistID() + ";");
 
 			st.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println(" NOT video deleted from playlist");
-			e.printStackTrace();
-		}
+		
 	}
 
-	public void viewVideo(Video video) {
+	public void viewVideo(Video video) throws SQLException {
 		String sql = "SELECT views FROM videos WHERE videos_id=" + video.getId() + ";";
 		if (allVideos.isEmpty()) {
-			PreparedStatement st;
-			try {
-				st = DBManager.getInstance().getConnection().prepareStatement(sql);
+			PreparedStatement st = DBManager.getInstance().getConnection().prepareStatement(sql);
 				ResultSet res = st.executeQuery();
 				while (res.next()) {
 					video.setId((res.getInt("videos_id") + 1));
 				}
-			} catch (SQLException e) {
-				System.out.println("problem in viewing a video");
-				e.printStackTrace();
-			}
+			
 		}
 	}
 	
-	public void commentVideo(Comment comment,Video video, User user){
-		//TODO DA OPRAVIM V DB  nqkoi danni za comment 
-		Connection con = DBManager.getInstance().getConnection();
-		PreparedStatement st;
-		try {
-			st = con.prepareStatement(
-					"INSERT INTO comments (users_user_id,video_id,content,postedDate) VALUES (?,?,?,?);");
-			st.setInt(1, user.getUserID());
-			st.setInt(2,video.getId());
-			st.setString(3, comment.getContent());
-			//TODO DATE THINGS
-		//	st.setTimestamp(4,comment.getPostedDate());
-			st.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("NOT commented video");
-			e.printStackTrace();
-		}
-	}
 	
-	public void deleteComment(Comment comment){
+	
+	public void deleteComment(Comment comment) throws SQLException{
 		Connection con = DBManager.getInstance().getConnection();
-		PreparedStatement st;
-		try {
-			st = con.prepareStatement("DELETE FROM comments WHERE comment_id= " +comment.getCommentID()+ ";");
-
+		PreparedStatement st = con.prepareStatement("DELETE FROM comments WHERE comment_id= " +comment.getCommentID()+ ";");
 			st.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println(" NOT comment deleted");
-			e.printStackTrace();
-		}
+		
 	}
 }
